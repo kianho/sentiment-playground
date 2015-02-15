734007a134dde491a651a7342f9520c723d23653
Kian Ho <hui.kian.ho@googlemail.com>
2015-02-15 16:43:45 +1100

    Updated do_pipeline.py
    Added code to:
    - compute the tf-idf feature vectors for a dataset.
    - train/pickle/unpickle a basic random forest model.
    
    This code will be incrementally improved as I refine the requirements.

--- do_pipeline.py
+++ do_pipeline.py
@@ -24,6 +24,13 @@ Options:
 Example workflow:
     ./do_pipeline.py < txt_sentoken.all.dat > txt_sentoken.dat
 
+TODO:
+    - user-specified classifiers
+    - user-specified classifier parameters.
+    - n_trees vs. oob accuracy plot for RF classifier
+    - saving/loading of trained models.
+    - classifier accuracy/ROC-AUC comparisons
+
 """
 
 import os
@@ -35,8 +42,6 @@ from collections import OrderedDict
 
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
-
-from sklearn.feature_extraction.text import CountVectorizer
 
 from docopt import docopt
 
@@ -80,6 +85,11 @@ def my_tokenizer(s):
 def iter_reviews(df, firstn=None, lastn=None):
     """
 
+    Arguments:
+        df --
+        firstn -- use only the first n sentences of each review.
+        lastn -- use only the last n sentences of each review.
+
     """
 
     for (label, rev_id), subdf in (df.groupby(["label", "rev_id"])):
@@ -114,5 +124,47 @@ if __name__ == '__main__':
 
         records.append((label, line))
 
-    data = pd.DataFrame.from_records(records, columns=["label", "text"])
-    data.to_csv(sys.stdout)
+    df = pd.DataFrame.from_records(records, columns=["label", "text"])
+    X, y = df.text, df.label
+
+    #data.to_csv(sys.stdout)
+
+    #
+    # Use the tf-idf statistic to weight the importance of each term.
+    #
+    from sklearn.cross_validation import cross_val_score
+    from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+    from sklearn.ensemble import RandomForestClassifier
+
+    count_vectoriser = CountVectorizer(tokenizer=my_tokenizer)
+    tfidf_transformer = TfidfTransformer(use_idf=False)
+
+    X_counts = count_vectoriser.fit(X)
+    X_counts = count_vectoriser.transform(X)
+
+    X_tfidf = tfidf_transformer.fit(X_counts)
+    X_tfidf = tfidf_transformer.transform(X_counts)
+
+    # Train the model
+    clf = RandomForestClassifier(n_estimators=200, oob_score=True, verbose=2, n_jobs=2)
+    clf.fit(X_tfidf.toarray(), y)
+
+    #
+    # muck around code below!
+    #
+
+    # Save (pickle) the model to disk
+    from sklearn.externals import joblib
+
+    # Note, sparse input matrices for RF's in sklearn not yet fully supported.
+    clf.fit(X_tfidf.toarray(), y)
+
+    joblib.dump(clf, "./mdl/polarity2.0.mdl")
+
+    del clf
+
+    # Load (unpickle) the model
+    clf = joblib.load("./mdl/polarity2.0.mdl")
+
+    #
+    #print cross_val_score(clf, X_tfidf.toarray(), y)
