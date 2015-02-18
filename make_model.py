92f01c77d5ecaa810425522a62ca57125255b0ca
Kian Ho <hui.kian.ho@googlemail.com>
2015-02-18 23:41:31 +1100

    Modified make_model.py
    Modified this script to write the model vocabulary (.mdl) to disk.

--- make_model.py
+++ make_model.py
@@ -37,7 +37,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.externals import joblib
 
 from docopt import docopt
-from utils import RE_SPACE
+from utils import normalize_tokens, make_tfidf_matrix
 
 DEFAULT_CLF_OPTS = \
         "RandomForestClassifier,n_estimators=200,oob_score=True,n_jobs=1"
@@ -48,28 +48,21 @@ if __name__ == '__main__':
     #DEFAULT_CLF_OPTS.split(",")
 
     df = pandas.read_csv(opts["-d"])
+    df.text = df.text.apply(lambda s : s.split())
     X, Y = df.text, df.label
 
     #
     # Use the tf-idf statistic to weight the importance of each term.
     #
-    count_vectoriser = CountVectorizer(tokenizer=lambda s : RE_SPACE.split(s))
-    tfidf_transformer = TfidfTransformer(use_idf=False)
-
-    X_counts = count_vectoriser.fit(X)
-    X_counts = count_vectoriser.transform(X)
-
-    # Save the vocabulary to disk
-    vocab = pandas.DataFrame(count_vectoriser.vocabulary_.items(),
-                columns=["term", "count"])
-    vocab.to_csv(opts["-V"], index=None)
-
-    X_tfidf = tfidf_transformer.fit(X_counts)
-    X_tfidf = tfidf_transformer.transform(X_counts)
+    X_tfidf, X_vocab = make_tfidf_matrix(X.tolist())
 
     # Train the model
     clf = RandomForestClassifier(n_estimators=10, oob_score=True, verbose=2, n_jobs=2)
     clf.fit(X_tfidf.toarray(), Y)
 
+    # Save the vocabulary to disk
+    vocab = pandas.DataFrame(X_vocab.items(), columns=["term", "index"])
+    vocab.to_csv(opts["-V"], index=None)
+
     # Save (pickle) the model to disk
-    joblib.dump(clf, opts["-o"])
+    joblib.dump(clf, opts["-m"])
