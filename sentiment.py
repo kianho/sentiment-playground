3341d4df1933fbc8dd78f2a2e0142003be2169be
Kian Ho <hui.kian.ho@googlemail.com>
2015-02-19 22:21:23 +1100

    Updated sentiment.py
    Added enough code to do basic training of models and classification.

--- sentiment.py
+++ sentiment.py
@@ -11,12 +11,27 @@ Author:
 Description:
     ...
 
+Usage:
+    sentiment.py train -c <csv> [-C <str>] -m <mdl> -V <csv> [-e <encoding>]
+    sentiment.py classify (-t <file> | --) -m <mdl> -V <csv> [-e <encoding>]
+
+Options:
+    -c <csv>        Training .csv file.
+    -C <str>        Classifier type [default: rf].
+    -m <mdl>        Output .mdl file.
+    -V <csv>        Output vocabulary .csv file.
+    -t <file>       File containing incoming text to be classified. 
+    -e <encoding>   File encoding [default: utf-8].
+    --              Read incoming text from stdin.
+
 """
 
 import os
 import sys
 import re
+import codecs
 import numpy
+import pandas
 
 from functools import reduce
 
@@ -27,6 +42,8 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.externals import joblib
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+from docopt import docopt
 
 
 RE_SPACE = re.compile(r"\s+")
@@ -53,6 +70,28 @@ STOP_WORDS = set(stopwords.words("english"))
 
 PORTER_STEMMER = PorterStemmer()
 
+COUNTER = 0
+
+
+def read_blob(buf, encoding):
+    if os.path.exists(buf):
+        with open(buf, "rb") as f:
+            str_val = codecs.getreader(encoding)(f).read()
+    elif hasattr(buf, "read"):
+        str_val = buf.read()
+    elif isinstance(buf, basestring):
+        str_val = buf
+    else:
+        raise NotImplementedError
+    return str_val
+
+
+def read_vocab(vocab_fn):
+    df = pandas.read_csv(vocab_fn, index_col=None, header=None)
+    return dict(df.to_records(index=None))
+
+
+
 def valid_word(w):
     """
     TODO:
@@ -66,69 +105,54 @@ def valid_word(w):
     ) and RE_ALPHA.match(w)
 
 
-# TODO: 
-# - convert these functions to return iterators
-
 def remove_non_words(tokens):
-    return [ t for t in tokens if valid_word(t) ]
+    return ( t for t in tokens if valid_word(t) )
 
 
 def remove_stop_words(tokens, stop_words=STOP_WORDS):
-    return [ t for t in tokens if t not in stop_words ]
+    return ( t for t in tokens if t not in stop_words )
 
 
 def stem_words(tokens):
-    return [ PORTER_STEMMER.stem(t) for t in tokens ]
+    return ( PORTER_STEMMER.stem(t) for t in tokens )
 
 
 def normalize_tokens(tokens):
     return [ t.lower() for t in remove_non_words(
-             remove_stop_words( stem_words(tokens))) ]
+             remove_stop_words(stem_words(tokens))) ]
 
 
-def preprocess_blob(blob, remove_html=False):
+def preprocess_blob(blob):
     """Preprocess raw text (e.g. from a text file, html page, twitter stream
     etc.) by normalising and tokenizing into a list of more usable document
     terms.
 
     Arguments:
         blob --
-        remove_html -- TODO, 
 
     Returns:
         a single list of normalised terms associated with a single document
         blob.
 
-    """
+    TODO:
+        - filter markup tokens
 
+    """
     tokens = reduce(lambda x, y : x + y,
-                map(word_tokenize, sent_tokenize(blob.lower())))
+                (word_tokenize(s) for s in sent_tokenize(blob.lower())))
     terms = normalize_tokens(tokens)
 
     return terms
 
 
-def make_tfidf_matrix(doc_term_lists, vocab=None):
+def make_tfidf_matrix(docs, vocab=None):
     """
-
-    Arguments:
-        doc_term_lists:
-            list of document term lists.
-
-        vocab (default: None):
-            ...
-
-    Returns:
-        ...
-
     """
-
-    X = numpy.array([ " ".join(terms) for terms in doc_term_lists ])
-    count_vectoriser = CountVectorizer(tokenizer=lambda s : s.split(), vocabulary=vocab)
+    count_vectoriser = CountVectorizer(tokenizer=preprocess_blob, vocabulary=vocab)
     tfidf_transformer = TfidfTransformer(use_idf=True)
 
-    X_counts = count_vectoriser.fit(X)
-    X_counts = count_vectoriser.transform(X)
+    X_counts = count_vectoriser.fit(docs)
+    X_counts = count_vectoriser.transform(docs)
 
     X_tfidf = tfidf_transformer.fit(X_counts)
     X_tfidf = tfidf_transformer.transform(X_counts)
@@ -136,46 +160,55 @@ def make_tfidf_matrix(doc_term_lists, vocab=None):
     return X_tfidf, count_vectoriser.vocabulary_
 
 
-def train_model(clf, dat_fn, mdl_fn, vocab_fn):
+def train_model(clf_type, csv_fn, mdl_fn, vocab_fn):
     """
-
-    Arguments:
-        clf --
-        dat_fn --
-        mdl_fn --
-        vocab_fn --
-
-    Returns:
-        ...
-
     """
-
-    df = pandas.read_csv(dat_fn)
-    df.text = df.text.apply(lambda s : s.split())
-    X, Y = df.text, df.label
+    df = pandas.read_csv(csv_fn, index_col=None, header=None)
+    X, Y = df[1], df[0]
 
     # Use the standard tf-idf matrix representation of the dataset.
-    X_tfidf, X_vocab = make_tfidf_matrix(X.tolist())
+    X_tfidf, X_vocab = make_tfidf_matrix(X)
+
+    if clf_type == "rf":
+        clf = RandomForestClassifier()
+    else:
+        raise NotImplementedError
 
     # Train the model
     clf.fit(X_tfidf.toarray(), Y)
 
     # Save the vocabulary to disk
-    vocab = pandas.DataFrame(X_vocab.items(), columns=["term", "index"])
-    vocab.to_csv(vocab_fn, index=None)
+    vocab = pandas.DataFrame(X_vocab.items())
+    vocab.to_csv(vocab_fn, index=None, header=None)
 
     # Save (pickle) the model to disk
     joblib.dump(clf, mdl_fn)
 
-    return
+    return clf, vocab
+
+
+def classify_text(buf, mdl_fn, vocab_fn, encoding):
+    """
+    """
+
+    blob = read_blob(buf, encoding=encoding)
+    vocab = read_vocab(vocab_fn)
+    docs = [blob]
+    X_tfidf, X_vocab = make_tfidf_matrix(docs, vocab=vocab)
+    clf = joblib.load(mdl_fn)
+
+    # TODO:
+    # - compare the model vocab with the incoming vocab.
+
+    return clf.predict(X_tfidf.toarray())
 
 
 if __name__ == "__main__":
-    #blob = "Good muffins cost $3.88\nin New York.  Please buy me\ntwo of them.\n\nThanks."
-    #print preprocess_blob(blob)
-    #print make_tfidf_matrix([ preprocess_blob(b) for b in (blob, blob) ])
-    dat_fn = 
-    clf = RandomForestClassifier()
-    train_model(clf, )
+    opts = docopt(__doc__)
 
-    #print clf
+    if opts["train"]:
+        train_model(opts["-C"], opts["-c"], opts["-m"], opts["-V"])
+    elif opts["classify"]:
+        classify_text(opts["-t"], opts["-m"], opts["-V"], opts["-e"])
+    else:
+        raise NotImplementedError
