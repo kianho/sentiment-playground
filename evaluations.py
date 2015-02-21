c478397398a18fa50a2dfb0c30e582b623e28f44
Kian Ho <hui.kian.ho@googlemail.com>
2015-02-21 19:51:11 +1100

    Updated evaluations.py
    Added code to compute a variety of metrics from a single ptab.

--- evaluations.py
+++ evaluations.py
@@ -29,8 +29,79 @@ import os
 import sys
 import pandas
 import sentiment as snt
+import sklearn
 
 from docopt import docopt
+from sklearn.metrics import roc_curve
+
+
+def do_roc_curve(y_correct, y_score):
+    """
+    """
+
+    import mpl_preamble
+    from mpl_preamble import pylab
+
+    fpr, tpr, thresh = roc_curve(y_correct, y_score, pos_label=1)
+
+    pylab.plot(x=fpr, y=tpr)
+    pylab.savefig("./roc.pdf")
+
+    return
+
+
+def calc_metrics(ptab):
+    """
+    """
+
+    correct, pred, scores = ptab[0], ptab[1], ptab[2]
+
+    # Compute the confusion matrix
+    confusion_mat = \
+        sklearn.metrics.confusion_matrix(correct, pred)
+
+    TP, FP = confusion_mat[1][1], confusion_mat[0][1]
+    TN, FN = confusion_mat[0][0], confusion_mat[1][0]
+
+    TPR = float(TP) / (TP + FN)
+    FPR = float(FP) / (FP + TN)
+    TNR = float(TN) / (FP + TN)
+    FNR = float(FN) / (FN + TP)
+
+    MCC = sklearn.metrics.matthews_corrcoef(correct, pred)
+    AUC = sklearn.metrics.roc_auc_score(correct, scores)
+
+    rec = float(TP) / (TP + FN)
+    prec = float(TP) / (TP + FP)
+
+    precision, recall, F1, sup = \
+        sklearn.metrics.precision_recall_fscore_support(correct, pred)
+
+    precision_neg, precision_pos = precision
+    recall_neg, recall_pos = recall
+
+    # accuracy
+    acc = sklearn.metrics.accuracy_score(correct, pred)
+
+    # error-rate
+    err = 1 - acc
+
+    # use the ptab file name as the mtab index
+    metrics = pandas.DataFrame([{}])
+
+    metrics["auc"] = AUC
+    metrics["mcc"] = MCC
+    metrics["precision"] = precision_pos
+    metrics["recall"] = recall_pos
+    metrics["specificity"] = TNR
+    metrics["tpr"] = TPR
+    metrics["fpr"] = FPR
+    metrics["tnr"] = TNR
+    metrics["fnr"] = FNR
+    metrics["err"] = err
+    metrics["acc"] = acc
+
+    return metrics
 
 
 def do_heldout(opts, ptab_fn):
@@ -43,22 +114,25 @@ def do_heldout(opts, ptab_fn):
     X_tfidf, X_vocab = snt.make_tfidf_matrix(X, toarray=True,
             vocabulary=vocabulary)
     y_pred, y_score = clf.predict(X_tfidf), clf.predict_proba(X_tfidf)
-    pos_score = pandas.Series(y_score[:,1])
 
-    # compute the ptab dataframe and write it to disk.
-    ptab_df = pandas.concat(
+    # compute the ptab and write it to disk.
+    ptab = pandas.concat(
             [pandas.Series(y),
              pandas.Series(y_pred),
              pandas.Series(y_score[:,1])], axis=1)
-    ptab_df.to_csv(ptab_fn, sep=" ", index=None, header=None)
+    ptab.to_csv(ptab_fn, sep=" ", index=None, header=None)
 
-    return ptab_df
+    return ptab
 
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
 
     if opts["heldout"]:
-        do_heldout(opts)
+        ptab = do_heldout(opts, "/dev/null")
+
+        # TODO: annotate each result row with classifier parameters and dataset
+        # name.
+        mtab = calc_metrics(ptab)
     elif opts["cv"]:
         raise NotImplementedError
