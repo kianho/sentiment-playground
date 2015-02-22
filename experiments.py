1955bb7c9437ecd64c7a10765aa0a81190fc2acf
Kian Ho <hui.kian.ho@googlemail.com>
2015-02-23 02:21:30 +1100

    Updated experiments.py
    Added wrapper class around GridSearchCV that allows multiple classifiers
    to be specified (in addition to the parameter grid).

--- experiments.py
+++ experiments.py
@@ -43,6 +43,60 @@ from sklearn.cross_validation import StratifiedKFold
 
 from docopt import docopt
 
+
+class MyGridSearchCV:
+    """
+    """
+
+    def __init__(self, param_grids, **kwargs):
+        """Constructor.
+
+        """
+
+        # Cross-validation generator and instantiate the folds in a list so we can
+        # re-use them for other estimators.
+        kwargs.setdefault("cv",
+                list(StratifiedKFold(y, n_folds=10, shuffle=True)))
+        kwargs.setdefault("n_jobs", -1)
+        kwargs.setdefault("scoring", "roc_auc")
+
+        self.param_grids_ = param_grids
+        self.grid_search_cv_kwargs_ = kwargs
+        self.grid_scores_ = []
+
+        return
+
+    def fit(self, X, y):
+        """
+        """
+
+        kwargs = self.grid_search_cv_kwargs_
+        class_name = lambda clf : clf.__class__.__name__
+
+        param_scores = []
+        for clf, param_grid in self.param_grids_:
+            cv_clf = GridSearchCV(clf, param_grid=param_grid, **kwargs)
+            cv_clf.fit(X, y)
+
+            param_scores.extend([
+                    { "fold_scores" : x.cv_validation_scores,
+                      "params" : x.parameters,
+                      "classifier" : clf.__class__.__name__ }
+                    for x in cv_clf.grid_scores_
+                ])
+
+        # Create a dataframe of grid scores for each possible
+        # classifier/parameter combination.
+        df = pandas.DataFrame(param_scores)
+        df["mean"] = df["fold_scores"].apply(lambda x : x.mean())
+
+        self.grid_scores_ = df
+
+        return
+
+
+
+
 # Initialise the global numpy random seed.
 numpy.random.seed(12345)
 
@@ -61,42 +115,23 @@ X, y = make_classification(n_samples=2000, n_features=50, scale=None, shift=None
 # Scale to unit variance with a 0 mean.
 X = sklearn.preprocessing.scale(X)
 
-# Cross-validation generator and instantiate the folds in a list so we can
-# re-use them for other estimators.
-cv_gen =  StratifiedKFold(y, n_folds=10, shuffle=True)
-cv_folds = list(cv_gen)
 
 # Perform a series of cross-validation grid searches over several different
 # types of classifiers.
 
-# Grid search over the SGDClassifier loss functions.
-sgdc_grid =  { "loss" : ["hinge", "log"] }
-sgdc_grid_clf = GridSearchCV(SGDClassifier(random_state=12345), param_grid=sgdc_grid, cv=cv_folds,
-        scoring="roc_auc", verbose=3, n_jobs=-1)
-sgdc_grid_clf.fit(X, y)
-
-# Grid search over naive Bayes (empty grid i.e. standard 10-fold cv).
-nbayes_clf = GridSearchCV(GaussianNB(), param_grid={}, cv=cv_folds,
-        scoring="roc_auc", verbose=3, n_jobs=-1)
-nbayes_clf.fit(X, y)
-
-# Random forest.
-_, n_feats = X.shape
-
-rf_grid_clf = GridSearchCV(RandomForestClassifier(n_estimators=1000),
-        param_grid={}, cv=cv_folds, scoring="roc_auc", verbose=3, n_jobs=-1)
-
-# Extremely random trees.
-ert_grid_clf = GridSearchCV(ExtraTreesClassifier(n_estimators=1000),
-        param_grid={}, cv=cv_folds, scoring="roc_auc", verbose=3, n_jobs=-1)
-
-# SVMs
 C = [.1, 1, 10, 100]
 svm_grid = [
         { "kernel" : ["linear"], "C" : C },
         { "kernel" : ["rbf"], "gamma" : [0.1, 0.2, 0.5, 1.0], "C" : C },
         { "kernel" : ["poly"], "degree" : [2,3], "C" : C }
     ]
-svm_grid_clf = GridSearchCV(SVC(), param_grid=svm_grid, scoring="roc_auc",
-        verbose=3, n_jobs=-1)
-print svm_grid_clf.fit(X, y).grid_scores_
+
+grid_search_cv = MyGridSearchCV([
+        (SGDClassifier(), { "loss" : ["hinge", "log"] }),
+        (GaussianNB(), {}),
+        (RandomForestClassifier(n_estimators=1000), {}),
+        (ExtraTreesClassifier(n_estimators=1000), {}),
+        (SVC(), svm_grid)
+    ], verbose=0)
+
+grid_search_cv.fit(X, y)
